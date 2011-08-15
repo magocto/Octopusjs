7d0fad6f49bc2bfea11cf463f76b5f6e86f688de
Jason Bennett <magocto@gmail.com>
2011-08-15 16:47:43 -0500

    more work on docs

--- octopus.js
+++ octopus.js
@@ -1,66 +1,80 @@
-/*!
- * Octupus.js v0.1
- * A object composition and namespacing library
- * http://octopusjs.org
- *
- * Copyright (c) 2011 Jason "Magocto" Bennett
- * Octopus may be freely distributed under the MIT license.
- * 
- * Dependencies:
- * Underscore.js
- * http://documentcloud.github.com/underscore/
- * 
- * 
- * @author Jason Bennett
- * @function
- */
+/* Octupus.js v0.1
+
+An object composition and namespacing library
+
+http://octopusjs.org (Comming soon)
+
+Copyright (c) 2011 Jason "Magocto" Bennett
+
+Octopus may be freely distributed under the MIT license.
+
+Dependencies:
+<a href="http://documentcloud.github.com/underscore/">Underscore.js</a>
+*/
 (function() {	
 	var root = this;
 	var oldOctopus = root.Octopus;
 	
 	
-	/*
-	 * @constructor 
-	 * 
-	 * @namespace Reference to Octopus, _8 is a shortcut
-	 */
+	// Base setup
+	// --------------
 	
+	// Octopus namespace and _8 shortcut
 	var Octopus, _8;
+	
+	// Export for either node or window
 	if (typeof exports !== 'undefined') {
 		Octopus = _8 = exports
 	} else {
 		Octopus = root._8 = root.Octopus = {};
 	}
 	
-	
+	// version
 	Octopus.VERSION = "0.1";
 
+	// Tentacles/Traits
+	// ----------
+	// mixed in objects to provide multiple inheritance.
+	
+	// a holder tentacle/traits
 	Octopus._traits = {};
 
-
-	/* @param {String} name
-	 * @param {Object) proto
+	// a function to add an trait<br />params
+	// 
+	/* 
+	 * name (String) a string identifier 
+	 * proto (Object) a object literal to hold our functionality
 	 */
 	Octopus.t =  function(name, proto) {
 		if(!name) throw "Octopus.t Invalid name"
 		if(!proto) return this._traits[id];
+		// if the tentacle has a construct function separate it to use when the main object constructor is called
 		var construct = proto.construct;
 		delete proto.construct;
 		this._traits[name] = new OctoTrait(proto, construct);		
 	}
-	
-	// check to see if a trait has been added
+	// check to see if a trait has been added<br /> params
+	/* 
+	 *  name (String) a string identifier
+	 */
 	Octopus.hasT = function(name) {
 		return (this._traits[name]);
 	}
-	Octopus._paths = {};
 	
-	// create object blueprint
-	Octopus.spawn = function(traits, proto) {
-		// create the a new base class each time so we can alter the prototype at will
 
+	// Object Composition 
+	// ----------
+	// create object blueprint<br />params
+	/*
+	 * traits (String|Array[String]) a list of tentacles to add
+	 * proto: (object) add on to the composite objects prototype. 
+	 */
+	// returns composite class definition
+	Octopus.spawn = function(traits, proto) {
+	
 		if(!proto) proto = {};
 	 	
+		// if the proto object has a construct function defined add it as our final constructor after all tentacle constructors have been called
 		var fn = this._getBaseObj(proto.construct);
 	
 		_.each( getTraitList(traits), function(id) {
@@ -71,11 +85,27 @@
 				_.extend(fn.prototype, t.proto);
 			}  
 		});
+		
+		// remove the construct function before the other methods are added to the composite objects prototype
 		delete proto.construct;
 		_.extend(fn.prototype, proto);
-
+	
+		// return the composite object blueprint
 		return fn;
 	}
+
+	// create and initialize <br />params
+	/* 
+	 * traits (String) or (Array[String]) a list of tentacles to add
+	 * proto: (Object) add on to the composite objects prototype. 
+	 * config: (Object) pass config object to newly constructed object
+	 */
+	Octopus.spawnSingle = function(traits, proto, config) {
+		var clz = Octopus.spawn(traits, proto);
+		return new clz(config);
+	}
+	
+	// create a new base class to add traits to
 	Octopus._getBaseObj = function(construct) {
 		var fn = function () {
 			 var self = this;
@@ -95,52 +125,80 @@
 		return fn;
 	}
 	
-	// a package system
+	// Namespacing
+	// ------------
+	// a object to hold our namespaces
+	Octopus._paths = {};
+	
+	/* create, get, or append a namespace
+	 @param p {String} a dot delimited path
+	 @param p
+	*/
 	Octopus.path = function(p, addon) {
 		if(!_.isString(p)) throw "Octopus space path must be a String";
 		return  Octopus._getPathObj(Octopus._paths, p.split("."), addon);
 	}
 	
+	
+	// recurse though namespace and get/create/append 
 	Octopus._getPathObj = function(parent, parts, addon) {
 		
 		var name = parts.shift();
 		var	current = parent[name];
 
 		// create a new package if the one we are looking for is not there
-		if (!current) 
-			parent[name] = current = {};
+		if (!current) parent[name] = current = {};
 			
 		// continue traversing if we are not at the bottom
-		if(parts.length) 
-			return Octopus._getPathObj(current, parts, addon);
+		if(parts.length) return Octopus._getPathObj(current, parts, addon);
 			
 		// at the last path extend if needed
-		if (addon) 
-			_.extend(current, addon);
-			
+		if (addon) _.extend(current, addon);
+		  
+		// always return an object	
 		return current;
 	}
 	
+
+	
+	// Dependency Injection
+	// --------
+	// run a anonymous function with Octopus.paths imported into the function scope
+	//
+	/*
+	 * imports (Object) a object in the form of importname:importpath
+	 * action (Function) a function to run in injected scope
+	 */
+	Octopus.go = function(imports, action) {
+		// create a empty function to hold scope
+		var scope = (function() {});
+		
+		// attach all the import paths to the scope
+		_.each(imports, function(p, name) {
+			scope[name] = Octopus.path(p);
+		});
+		// run the anonymous function in the scope
+		if(_.isFunction(action)) action.apply(scope, Array.prototype.slice.call(arguments, 2))
+	}
+	
+	// Utils
+	// ---------
+	
+	// a noCoflict so we can play nicely with other octopi
 	Octopus.noConflict =  function() {
 		root.Octopus = root._8 = oldOctopus;
 		return Octopus;
 	}
 	
-	// extend the Octopus namespace
+	// extend the Octopus namespace and add functionality to Octopus itself
 	Octopus.learn = function(ext) {
 		_.each(_.functions(ext), function(name){
 			Octopus[name] = ext[name];
 	    });
 	};
 	
-	// run a anynomous function
-	Octopus.go = function(imports, action) {
-		var scope = (function() {});
-		_.each(imports, function(p, name) {
-			scope[name] = Octopus.path(p);
-		});
-		if(_.isFunction(action)) action.apply(scope, Array.prototype.slice.call(arguments, 2))
-	}
+	// Helpers
+	// ---------
 	
 	// a little fly weight obj to hold traits
 	var OctoTrait = function(proto, init) {
@@ -148,12 +206,14 @@
 		this.init = init;
 	}
 	
+	// returns an array of trait ids from either a comma delimited string or an array of strings
 	var getTraitList = function(traits) {
 		if(_.isArray(traits)) return traits;
 		if(_.isString(traits)) return _.map(traits.split(","), function(t) {return trim(t)});
 		return [];
 	} 
 	
+	// string trim
 	var trim = function(str) {
 		return str.replace(/^\s*([\S\s]*?)\s*$/, '$1');
 	}
